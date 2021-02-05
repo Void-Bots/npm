9e4f7541ceadb22f01b48028e90aa19ff8a15593
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2021-02-06 00:32:05 +0530

    Added typings, fixed Voidbots! (#2)
    * Update index.js
    * Create index.d.ts
    * Update package.json
    * Update index.js
    * Update README.md

---
+++ index.d.ts
@@ -0,0 +1,71 @@
+import { EventEmitter } from 'events';
+
+export interface Options{
+    statsInterval?: number;
+}
+
+export interface Bot{
+    botid: string;
+    votes: number;
+    servers: string;
+    monthly_votes: number;
+    owners: string[];
+    longdesc: string;
+    shortdesc: string;
+    certified: string;
+    links: {
+        invite: string;
+        supportserver?: string;
+        website?: string;
+        github?: string;
+        donate?: string;
+    }
+}
+
+export interface BotReviews{
+    reviews: {
+        rating: string;
+        responses: any[];
+        flagged: boolean;
+        createdAt: string;
+        userid: string;
+        botid: string;
+        message: string;
+    }[];
+}
+
+export interface BotAnalytics{
+    build: string;
+    analytics: {
+        date: string;
+        unique_views: number;
+        views: number;
+        votes: number;
+        reviews: number;
+        referrers: {
+            name: string;
+            value: number;
+        }[];
+        countries: {
+            name: string;
+            value: number;
+        }[];
+    }[];
+}
+
+export type Response<T> = T | { message: string } | { error: string };
+
+export default class VoidBots extends EventEmitter{
+    
+    public static version: string;
+    public token: string;
+
+    public constructor(token: string, options?: any, client?: any);
+    public postStats(serverCount: number, shardCount?: number): Promise<string>;
+    public hasVoted(id: string): Promise<string>;
+    public getBotInfo(id: string): Promise<Response<Bot>>;
+    public getReviews(id: string): Promise<Response<BotReviews>>;
+    public getAnalytics(id: string): Promise<Response<BotAnalytics>>;
+    public tokenAvailable(): never | boolean;
+
+}
--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const isLib = (library, client) => {
 const isASupportedLibrary = client => isLib('discord.js', client) || isLib('eris', client);
 
 class VoidBots extends EventEmitter {
+	
+	
+   static version = require('./package.json').version;
+
    /**
    * Creates a new VoidBots Instance.
    * @param {string} token Your voidbots.net token for this bot.
@@ -138,10 +142,11 @@ class VoidBots extends EventEmitter {
       }).then(res => res.json());
     }
 
-   static tokenAvailable() {
+   tokenAvailable() {
      if (!this.token) throw new ReferenceError('No VoidBots token found in this instance');
      return true;
    }
 
 }
+
 module.exports = VoidBots;
