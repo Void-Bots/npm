44a1be16bbc58501dec4ab0d21a79beb12ffb016
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-04-05 16:39:47 -0300

    Fix the errors so users know its from the npm (#8)
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    Co-authored-by: DanPlayz <damuffin36@gmail.com>

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class VoidBots extends EventEmitter {
    */
     constructor(token, options, client) {
       super();
-      if (typeof token !== "string") throw new TypeError("Argument 'token' must be a string");
+      if (typeof token !== "string") throw new TypeError("[Void bots] Argument 'token' must be a string");
       Object.defineProperty(this, "token", {
         value: token,
         enumerable: false,
@@ -40,9 +40,9 @@ class VoidBots extends EventEmitter {
         options = {};
       }
       this.options = options ?? {};
-      if (!(client && isASupportedLibrary(client))) throw "Argument 'client' must be a client instance of a supported library (Discord.js or Eris)";
+      if (!(client && isASupportedLibrary(client))) throw "[VoidBots] Argument 'client' must be a client instance of a supported library (Discord.js or Eris)";
       if (typeof this.options.statsInterval !== "number") this.options.statsInterval = 1800000;
-      if (this.options.statsInterval < 900000) throw new RangeError("'options.statsInterval' may not be shorter than 900000 milliseconds (15 minutes)");
+      if (this.options.statsInterval < 900000) throw new RangeError("[VoidBots] 'options.statsInterval' may not be shorter than 900000 milliseconds (15 minutes)");
 
       /**
        * Event that fires when the stats have been posted successfully by the autoposter.
@@ -120,7 +120,7 @@ class VoidBots extends EventEmitter {
     }
 
     async _webhookServer() {
-      if(this.Fudshjifgsdujytfryoiklajsdhnigdtswkuidfhbjsrfytusahkjhvf_FireOnce) throw Error('This function may only be run once.');
+      if(this.Fudshjifgsdujytfryoiklajsdhnigdtswkuidfhbjsrfytusahkjhvf_FireOnce) throw Error('[Void bots] This function may only be run once.');
       this.Fudshjifgsdujytfryoiklajsdhnigdtswkuidfhbjsrfytusahkjhvf_FireOnce = true;
       let localtunnel, express;
       try {
@@ -169,7 +169,7 @@ class VoidBots extends EventEmitter {
    }
 
    tokenAvailable() {
-     if (!this.token) throw new ReferenceError("No VoidBots token found in this instance.");
+     if (!this.token) throw new ReferenceError("[VoidBots] No VoidBots token found in this instance.");
      this.checkAuth()
      return true;
    }
