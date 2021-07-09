deb13dbb6869a13ea1ee5f87525af1ef783cd42d
DanPlayz <12685647+DanPlayz0@users.noreply.github.com>
2021-07-09 00:24:20 -0700

    Fixed RangeError: Maximum call stack size exceeded

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ class VoidBots extends EventEmitter {
         options = {};
       }
       this.options = options ?? {};
-      if (!(client && isASupportedLibrary(client))) throw "[VoidBots] Argument 'client' must be a client instance of a supported library (Discord.js or Eris)";
       if (typeof this.options.statsInterval !== "number") this.options.statsInterval = 1800000;
       if (this.options.statsInterval < 900000) throw new RangeError("[VoidBots] 'options.statsInterval' may not be shorter than 900000 milliseconds (15 minutes)");
 
@@ -58,17 +57,16 @@ class VoidBots extends EventEmitter {
 
       this.client = client;
       this.client.once("ready", async () => {
-        this.checkAuth()
         if(this.options.webhookEnabled) this._webhookServer();
       	if(this.options.autoPost) {
           async function post(vbClass) {
-            return vbClass.postStats()
+            return vbClass.postStats(vbClass.client ? (vbClass.client.guilds.size ?? vbClass.client.guilds.cache.size ?? 0) : 0, vbClass.client.shard.count ?? 0 )
             .then(() => vbClass.emit("posted"))
             .catch((e) => vbClass.emit("error", e));
           }
           post(this);
           setInterval(post, this.options.statsInterval);
-    	}
+        }
       });
     }
 	
@@ -78,18 +76,12 @@ class VoidBots extends EventEmitter {
      * @param {number} [shardCount] The count of all shards of your bot.
      * @returns {string}
      */
-    async postStats(serverCount, shardCount = 0) {
+    async postStats(server_count = 0, shard_count = 0) {
       this.tokenAvailable();
-      if (!this.client) {
-        if (typeof serverCount !== "number") throw new TypeError("[VoidBots → postStats()] Argument 'serverCount' must be a number.");
-        if (typeof shardCount !== "number") throw new TypeError("[VoidBots → postStats()] Argument 'shardCount' must be a number.");
-      }
-      const data = {
-        server_count: this.client ? (this.client.guilds.size ?? this.client.guilds.cache.size) : serverCount,
-        shard_count: this.client?.shards?.size > 1 ? this.client?.shard?.count : shardCount
-      };
+      if (typeof server_count !== "number") throw new TypeError("[VoidBots → postStats()] Argument 'serverCount' must be a number.");
+      if (typeof shard_count !== "number") throw new TypeError("[VoidBots → postStats()] Argument 'shardCount' must be a number.");
 
-      return this._request(`/bot/stats/${this.client.user.id}`, "POST", data).then((res) => res.text());
+      return this._request(`/bot/stats/${this.client.user.id}`, "POST", { server_count, shard_count }).then((res) => res.text());
     }
 	
     /**
@@ -97,34 +89,39 @@ class VoidBots extends EventEmitter {
      * @param {string} id The ID of the user to check for.
      * @returns {string} The JSON content from the API.
      */
-    async hasVoted(id) {
+    async hasVoted(userid) {
       this.tokenAvailable();
-      return this._request(`/bot/voted/${this.client.user.id}/${id}`, "GET").then((res) => res.text());
+      return this._request(`/bot/voted/${this.client.user.id}/${userid}`, "GET").then((res) => res.text());
     }
-    async checkAuth() {
-      this.tokenAvailable();
-      let data = await this._get(`/bot/info/${this.client.user.id}`, "GET").then((res) => res.json());
-      if (data.message === 'Invalid authorization key provided') throw Error('[VoidBots] Token is not valid')
-    }
-
-    async getBotInfo(id) {
-      this.tokenAvailable();
-      return this._request(`/bot/info/${id}`, "GET").then((res) => res.json());
-    }
-
+    
     async getReviews() {
       this.tokenAvailable();
       return this._request(`/bot/reviews/${this.client.user.id}`).then((res) => res.json());
     }
-
+    
     async getAnalytics() {
       this.tokenAvailable();
       return this._request(`/bot/analytics/${this.client.user.id}`).then((res) => res.json());
     }
+    
+    async getBot(botid) {
+      this.tokenAvailable();
+      return this._get(`/bot/info/${botid}`).then((res) => res.json());
+    }
 
+    async getPack(packid) {
+      this.tokenAvailable();
+      return this._get(`/pack/info/${packid}`).then((res) => res.json());
+    }
+
+    async getUser(userid) {
+      this.tokenAvailable();
+      return this._get(`/user/info/${userid}`).then((res) => res.json());
+    }
+    
     async _webhookServer() {
-      if(this.Fudshjifgsdujytfryoiklajsdhnigdtswkuidfhbjsrfytusahkjhvf_FireOnce) throw Error('[Void bots] This function may only be run once.');
-      this.Fudshjifgsdujytfryoiklajsdhnigdtswkuidfhbjsrfytusahkjhvf_FireOnce = true;
+      if(this.hasWebFired_FireOnce) throw Error('[VoidBots → _webhookServer()] This function may only be run once.');
+      this.hasWebFired_FireOnce = true;
       let localtunnel, express;
       try {
         localtunnel = require('localtunnel'), express = require('express');
@@ -143,40 +140,38 @@ class VoidBots extends EventEmitter {
       });
       app.listen(port, async () => { console.log(`Webhook server initialized`) });
     }
-
+    
     _createKey(len=44) {
       var text = "";
       var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
       for (var i = 0; i < len; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
       return text;
     }
+    
+    _request(url, type = "POST", data = {}) {
+      return fetch(`${baseURL}${url}`, {
+        method: type,
+        headers: { 
+          Authorization: this.token,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+    }
 
-   _request(url, type = "POST", data = {}) {
-    return fetch(`${baseURL}${url}`, {
-      method: type,
-      headers: { 
-        Authorization: this.token,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-   }
-   _get(url) {
-    return fetch(`${baseURL}${url}`, {
-      method: 'GET',
-      headers: { 
-        Authorization: this.token,
-        "Content-Type": "application/json"
-      },
-    })
-   }
-
-   tokenAvailable() {
-     if (!this.token) throw new ReferenceError("[VoidBots] No VoidBots token found in this instance.");
-     this.checkAuth()
-     return true;
-   }
-
-}
-
-module.exports = VoidBots;
+    _get(url) {
+      return fetch(`${baseURL}${url}`, {
+        method: 'GET',
+        headers: { 
+          Authorization: this.token,
+          "Content-Type": "application/json"
+        },
+      })
+    }
+    
+    tokenAvailable() {
+      if (!this.token) throw new ReferenceError("[VoidBots] No VoidBots token found in this instance.");
+      return true;
+    }
+  }
+  module.exports = VoidBots;
